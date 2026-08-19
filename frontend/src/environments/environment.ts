a3f4e0778948cccbb1eed970c05c5920f431a624
Eugen Io <io.eugen@gmail.com>
2026-08-19 10:55:50 +0200

    🔖 Deploy V2026.08.19.19

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    version: 'V2026.08.19.18',
+    version: 'V2026.08.19.19',
     apiUrl: '/api',
     backupApiUrl: '/api',
     supabaseUrl: 'https://fkcktcwtnmuflasiueji.supabase.co',
